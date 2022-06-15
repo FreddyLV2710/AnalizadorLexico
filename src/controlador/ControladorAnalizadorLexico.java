9fb90154a1f06d0221eea510a2dcef7a1111511e
rodri <rodri@LAPTOP-GKGUJHOM>
2022-06-15 16:28:16 -0500

    Creacion de clases

---
+++ src/controlador/ControladorAnalizadorLexico.java
@@ -0,0 +1,17 @@
+
+package controlador;
+
+import vista.frmAnalizadorLexico;
+
+
+public class ControladorAnalizadorLexico {
+    private frmAnalizadorLexico vista;
+
+    public ControladorAnalizadorLexico(frmAnalizadorLexico vista) {
+        this.vista = vista;
+        
+        vista.btnAnalizar.addActionListener(l);
+    }
+    
+    
+}
